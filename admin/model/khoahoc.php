e5e6840a285fae5c30c1d0a3e08982369fd14845
Duyhlph22355 <115914772+Duyhlph22355@users.noreply.github.com>
2022-11-20 20:24:35 +0700

    them function update_khoahoc()

--- admin/model/khoahoc.php
+++ admin/model/khoahoc.php
@@ -6,4 +6,11 @@ function insert_khoahoc($ten_khoa_hoc,$anh, $gia, $thoi_gian_hoc, $mo_ta, $id_da
 }
 //xoa sanpham
 
+// update khoa hoc
+function update_khoahoc($ten_khoa_hoc, $anh, $gia, $thoi_gian_hoc, $mo_ta, $id_danh_muc, $id_lop, $id_khoa_hoc)
+{
+    $sql = "UPDATE khoa_hoc SET ten_khoa_hoc='$ten_khoa_hoc', anh_khoa_hoc='$anh', gia='$gia', thoi_gian_hoc='$thoi_gian_hoc', mo_ta='$mo_ta', id_danh_muc='$id_danh_muc',id_lop='$id_lop' WHERE id_khoa_hoc='$id_khoa_hoc'";
+    pdo_execute($sql);
+}
+
 ?>
